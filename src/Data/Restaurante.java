d789292aa3978d19094aaedc1aaffb823394aacf
Zivlew <94771636+Zivlew@users.noreply.github.com>
2021-11-22 02:43:32 -0500

    Add files via upload
    Se agrego la UI y se reemplazo el nombre de la clase linkedlist para su revisión ya que no se podian obtener datos y algunas funciones no tenian un adecuado funcionamiento

--- src/Data/Restaurante.java
+++ src/Data/Restaurante.java
@@ -3,8 +3,8 @@ import Structures.*;
 public class Restaurante {
 private String nombre;
 private String direccion;
-LinkedList<Plato> platos = new LinkedList<>();
-LinkedList<Bebida> bebidas = new LinkedList<>();
+LinkList<Plato> platos = new LinkList<>();
+LinkList<Bebida> bebidas = new LinkList<>();
 public String getNombre() {
 	return nombre;
 	
@@ -15,27 +15,32 @@ public String getDireccion() {
 public void setDireccion(String direccion) {
 	this.direccion = direccion;
 }
-public LinkedList<Plato> getPlatos() {
+public LinkList<Plato> getPlatos() {
 	return platos;
 }
-public void setPlatos(LinkedList<Plato> platos) {
+public void setPlatos(LinkList<Plato> platos) {
 	this.platos = platos;
 }
-public LinkedList<Bebida> getBebidas() {
+public LinkList<Bebida> getBebidas() {
 	return bebidas;
 }
-public void setBebidas(LinkedList<Bebida> bebidas) {
+public void setBebidas(LinkList<Bebida> bebidas) {
 	this.bebidas = bebidas;
 }
 public void setNombre(String nombre) {
 	this.nombre = nombre;
 }
-public Restaurante(String nombre, String direccion, LinkedList<Plato> platos, LinkedList<Bebida> bebidas) {
+public Restaurante(String nombre, String direccion, LinkList<Plato> platos, LinkList<Bebida> bebidas) {
 	super();
 	this.nombre = nombre;
 	this.direccion = direccion;
 	this.platos = platos;
 	this.bebidas = bebidas;
+}
+public Restaurante(String nombre, String direccion) {
+	super();
+	this.nombre = nombre;
+	this.direccion = direccion;
 }
 public void crearPlato(String nombre, String descripcion, int precio) {
 	Plato nuevo = new Plato(nombre,descripcion,precio);
